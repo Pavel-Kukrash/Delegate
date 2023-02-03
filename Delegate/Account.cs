524fb2bff1b796c15e25014737280e97c7ca61f2
kukra <kukra@LAPTOP-V5T7B30B>
2023-02-02 23:58:59 -0500

    Add project files.

---
+++ Delegate/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+
+{
+
+    public delegate void AccountHandler(string message);
+    public delegate void NoParameters();
+    class Account 
+    {
+        public int deposit { get; set; }
+        public string Description { get; set; }
+
+        AccountHandler? taken;
+        public void RegisterHandler(AccountHandler del) => taken = del;
+
+        public Account() { }
+        public Account(string description) => Description = description;
+        public Account(int dep) => deposit = dep;
+
+        public void Add(int add) => deposit += add;
+
+        public void Take(int take)
+        {
+
+
+            if (deposit >= take)
+            {
+                deposit -= take;
+                //Console.WriteLine($"Withdrawal {take} USD");
+                taken?.Invoke($"Withdrawal {take} USD");
+            }
+            else
+            {
+                //Console.WriteLine($"Transaction is cancelled, cash shortgage is {deposit - take}");
+                taken?.Invoke($"Transaction is cancelled, cash shortgage is {deposit - take}");
+            }
+        }
+
+        public int Result()
+        {
+            return deposit;
+        }
+
+     }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using Delegate;
+using System.Collections;
+
+Account account = new Account(200);
+account.RegisterHandler(PrintSimpleMessage);
+
+account.Add(100);
+account.Take(550);
+
+Console.WriteLine($"Your balance after transaction is {account.Result()}");
+
+
+NoParameters noPrmt = new(ShowMessage);
+noPrmt += SendEmail;
+noPrmt();
+
+
+void PrintSimpleMessage(string message) => Console.WriteLine(message);
+void ShowMessage() => Console.WriteLine("Thanks for transaction");
+void SendEmail() => Console.WriteLine("Confirmation sent on your email");
+
+
+var accounts = new List<string>() {"Checking", "Checking Premium", "Saving", "Investing", "Investing2" };
+accounts.Add("Inv3");
+
+var selectAccount = accounts.Where(a => a.ToUpper().StartsWith("C")).
+    OrderBy(a => a);
+
+Console.WriteLine("This checking accounts are available: ");
+
+foreach (var acc in selectAccount)
+{
+    Console.WriteLine(acc);
+}
+
+
+
+Console.ReadKey();
